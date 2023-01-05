d87313147d83a6e365175d8656099d84b6f58421
CameronStClair95 <cameronaitcheson@gmail.com>
2023-01-05 20:56:30 +0000

    stuck on iteration 3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movies.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+// router.get('/movies', (req, res) => {
+//     Movie.find()
+//         .then(result) => {
+//             console.log(result)
+//             res.render('movies.hbs', result)
+//         })
+//         .catch(error => {
+//             console.log(error)
+//         })
+
+router.get('/movies', (req, res) => {
+    Movie.find()
+      .then(movies => {
+        res.render('movies', { movies });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+    
 
 module.exports = router;
